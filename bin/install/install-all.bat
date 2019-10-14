e6cbf17c69b4bd2c81f9925fde3f9c7640afce98
Will Reynolds <will.rnlds@gmail.com>
2019-10-13 20:26:09 -0500

    updated installer for ycm and some other apps

--- bin/install/install-all.bat
+++ bin/install/install-all.bat
@@ -1,12 +1,50 @@
 setlocal EnableDelayedExpansion
 
-:: install package manager and applications
-PowerShell -NoProfile -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
+:: install chocolatey package manager
+PowerShell -NoProfile -Command ^
+    "Set-ExecutionPolicy Bypass -Scope Process -Force; ^
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
 call %programdata%\chocolatey\bin\RefreshEnv.cmd
 
 :: install programs
-choco install -y git curl vim
+choco install -y ^
+    git ^
+    curl ^
+    mingw ^
+    msys2 ^
+    llvm ^
+    python3 ^
+    ruby ^
+    autohotkey ^
+    vim ^
+    notepadplusplus ^
+    programmer-dvorak ^
+    vscode ^
+    kdiff3 ^
+    visualstudio2019-workload-vctools ^
+    cmake ^
+	firefox
+    
 call RefreshEnv
+
+:: install myconf
+set myconfpath="%userprofile%\.myconf"
+git clone https://github.com/willr001/.myconf temp-myconf
+xcopy temp-myconf\.git %myconfpath% /E /H /I /K
+rmdir /s /q temp-myconf
+git --git-dir=%myconfpath% --work-tree="%userprofile%" reset --hard
+mklink /J "%userprofile%\vimfiles" "%userprofile%\.vim"
+
+:: put user/bin in path
+set "pathToInsert=%userprofile%\bin"
+if "!path:%pathToInsert%=!" equ "%path%" (
+   setx PATH "%PATH%;%pathToInsert%"
+)
+:: put cmake/bin in path
+set "pathToInsert=%programfiles%\CMake\bin"
+if "!path:%pathToInsert%=!" equ "%path%" (
+   setx PATH "%PATH%;%pathToInsert%"
+)
 
 :: install fancy terminal fonts
 if not exist "%appdata%\my-conf\powerline-fonts" (
@@ -16,23 +54,14 @@ if not exist "%appdata%\my-conf\powerline-fonts" (
     rmdir /s /q powerline-fonts
 )
 
-:: install myconf
-set myconfpath="%userprofile%\.myconf"
-git clone https://github.com/willr001/.myconf temp-myconf
-xcopy temp-myconf\.git %myconfpath% /E /K /H
-rmdir /s /q temp-myconf
-git --git-dir=%myconfpath% --work-tree="%userprofile%" reset --hard
-mklink /J "%userprofile%\vimfiles" "%userprofile%\.vim"
-
-:: make sure user bin is in path
-set "pathToInsert=%userprofile%\bin"
-if "!path:%pathToInsert%=!" equ "%path%" (
-   setx PATH "%PATH%;%pathToInsert%"
-)
-
 :: install vundle.vim and plugins
 git clone https://github.com/VundleVim/Vundle.Vim "%userprofile%\vimfiles\bundle\Vundle.vim"
 vim +PluginInstall +qall
 
+:: compile ycm
+cd %userprofile%\vimfiles\bundle\youcompleteme
+python install.py --clang-completer
+
 :: done
+pause
 exit /b
