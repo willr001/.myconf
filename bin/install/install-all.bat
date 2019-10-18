a7f709f60ccff1dd703d9f5c4b8fb07fd479aa8f
Will Reynolds <will.rnlds@gmail.com>
2019-10-17 22:21:34 -0500

    installer changes

--- bin/install/install-all.bat
+++ bin/install/install-all.bat
@@ -1,92 +1,102 @@
 setlocal EnableDelayedExpansion
+setlocal EnableExtensions
 
 :: install chocolatey package manager
 PowerShell -NoProfile -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
 call "%programdata%\chocolatey\bin\RefreshEnv.cmd"
 
 :: install programs
-choco upgrade -y ^
-    7zip ^
-    activeperl ^
-    activetcl ^
-    astyle ^
-    autohotkey ^
-    cccp ^
-    CMake ^
-    ConEmu ^
-    cppcheck ^
-    curl ^
-    cutepdf ^
-    deluge ^
-    doxygen ^
-    dropbox ^
-    Everything ^
-    Firefox ^
-    freecommander ^
-    Ghostscript.app ^
-    git ^
-    GoogleChrome ^
-    googledrive ^
-    HexEdit ^
-    irfanview ^
-    kdiff3 ^
-    libreoffice ^
-    lua ^
-    mc ^
-    mingw ^
-    openvpn ^
-    paint.net ^
-    PDFXchangeEditor ^
-    PDFXChangeViewer ^
-    picasa ^
-    programmer-dvorak ^
-    putty ^
-    python2 ^
-    python3 ^
-    racket ^
-    reshack ^
-    ruby ^
-    steam ^
-    thunderbird ^
-    tor-browser ^
-    tortoisehg ^
-    vim ^
-    virtualbox ^
-    visualstudio2019-workload-vctools ^
-    vlc ^
-    wget ^
-    windirstat ^
-    XmlNotepad
+choco upgrade -y 7zip
+choco upgrade -y activeperl
+choco upgrade -y activetcl
+choco upgrade -y astyle
+choco upgrade -y autohotkey
+choco upgrade -y cccp
+choco upgrade -y CMake
+choco upgrade -y ConEmu
+choco upgrade -y cppcheck
+choco upgrade -y curl
+choco upgrade -y cutepdf
+choco upgrade -y deluge
+choco upgrade -y doxygen.install
+choco upgrade -y dropbox
+choco upgrade -y Everything
+choco upgrade -y Firefox
+choco upgrade -y freecommander
+choco upgrade -y Ghostscript.app
+choco upgrade -y git
+choco upgrade -y GoogleChrome
+choco upgrade -y googledrive
+choco upgrade -y HexEdit
+choco upgrade -y irfanview
+choco upgrade -y kdiff3
+choco upgrade -y libreoffice
+choco upgrade -y lua
+choco upgrade -y mc
+choco upgrade -y mingw
+choco upgrade -y msys2
+choco upgrade -y openvpn
+choco upgrade -y paint.net
+choco upgrade -y PDFXchangeEditor
+choco upgrade -y PDFXChangeViewer
+choco upgrade -y picasa
+choco upgrade -y programmer-dvorak
+choco upgrade -y putty
+choco upgrade -y python2
+choco upgrade -y python3
+choco upgrade -y racket
+choco upgrade -y reshack
+choco upgrade -y ruby
+choco upgrade -y steam
+choco upgrade -y thunderbird
+choco upgrade -y tor-browser
+choco upgrade -y tortoisehg
+choco upgrade -y vim
+choco upgrade -y virtualbox
+choco upgrade -y visualstudio2019-workload-vctools
+choco upgrade -y vlc
+choco upgrade -y wget
+choco upgrade -y windirstat
+choco upgrade -y XmlNotepad
     
 call RefreshEnv
 
-:: install myconf
-set myconfpath="%userprofile%\.myconf"
-rmdir /s /q temp-myconf
-rmdir /s /q %myconfpath%
-git clone https://github.com/willr001/.myconf temp-myconf
-xcopy temp-myconf\.git %myconfpath% /E /H /I /K
-rmdir /s /q temp-myconf
-git --git-dir=%myconfpath% --work-tree="%userprofile%" reset --hard
-mklink /J "%userprofile%\vimfiles" "%userprofile%\.vim"
-mklink "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\AutoHotKey.exe" "%programfiles%\AutoHotkey\AutoHotkey.exe"
+:: set numlock on with windows boot
+REG ADD "HKCU\.DEFAULT\Control Panel\Keyboard"^
+ /v "InitialKeyboardIndicators" /t REG_SZ /d 2 /f
+:: remove all pinned items from taskbar
+DEL /F /S /Q /A "%AppData%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\*"
+REG DELETE HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband /F
+:: removes the onedrive icon from the nav pane
+REG ADD "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}"^
+ /v "System.IsPinnedToNameSpaceTree" /t REG_DWORD /d 00000000 /f
+:: removes dropbox icon from nav pane
+REG ADD "HKEY_CLASSES_ROOT\CLSID\{E31EA727-12ED-4702-820C-4B6445F28E1A}"^
+ /v "System.IsPinnedToNamespaceTree" /t REG_DWORD /d 00000000 /f
+:: disables hibernate to save RAM_SIZE on C:
+POWERCFG /h off
 
 :: put user/bin in path
+mkdir "%userprofile%\bin"
 set "pathToInsert=%userprofile%\bin"
-if "!path:%pathToInsert%=!" equ "%path%" (
-   setx PATH "%PATH%;%pathToInsert%"
-)
-
-:: put chocolatey tools in path
-set "pathToInsert=%programdata%\chocolatey\tools"
 if "!path:%pathToInsert%=!" equ "%path%" (
    setx PATH "%PATH%;%pathToInsert%"
 )
 
 call refreshenv
 
-SHIMGEN -o "%userprofile%\bin\reshack.exe" -p "%programfiles(x86)%\Resource Hacker\ResourceHacker.exe" --gui
-SHIMGEN -o "%userprofile%\bin\cmake.exe"   -p "%programfiles%\CMake\bin\cmake.exe"
+echo "%programdata%\chocolatey\tools\shimgen.exe" -o "%%~dp0%%~n1.exe" -p "%%~f1" %%2 >"%%userprofile%%\bin\shim.cmd"
+shim "%programfiles%\CMake\bin\cmake.exe"
+
+:: install myconf
+set "myconf=.myconf"
+rmdir /s /q "%userprofile%\%myconf%"
+git clone --bare https://github.com/willr001/%myconf% "%userprofile%\%myconf%"
+ECHO git --git-dir="%userprofile%\%myconf%" --work-tree="%userprofile%" %%* >"%userprofile%\bin\myconf.cmd"
+call myconf reset --hard
+call myconf config --local status.showUntrackedFiles no
+mklink /J "%userprofile%\vimfiles" "%userprofile%\.vim"
+mklink "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\AutoHotKey.exe" "%programfiles%\AutoHotkey\AutoHotkey.exe"
 
 :: install ceedling
 SET "olddir=%cd%"
